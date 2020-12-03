0e28b5e1dfe288ce5910b3dabfcffd670bc30253
vicky17d <vicky17d@gmail.com>
2020-12-02 18:51:27 -0800

    Reproduce IDEA-256757: Open option not visible in dropdown

---
+++ src/main/kotlin/com/github/vicky17d/issuereproducer/welcomescreenactions/BarAction.kt
@@ -0,0 +1,10 @@
+package com.github.vicky17d.issuereproducer.welcomescreenactions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class BarAction: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
